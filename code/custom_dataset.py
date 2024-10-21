8ab07683c3ed8616d92113c3fd0204f586faac8c
Hafeez Ullah <hafizhafeezullah05@gmail.com>
2024-10-21 16:47:23 +0200

    new commits

--- code/custom_dataset.py
+++ code/custom_dataset.py
@@ -1,10 +1,10 @@
-# custom_dataset.py
 import os
 import pandas as pd
 from PIL import Image
 from sklearn.preprocessing import LabelEncoder
 import torch
 from torch.utils.data import Dataset
+from torchvision import transforms
 
 class CustomMelanomaDataset(Dataset):
     def __init__(self, csv_file, image_dir, transform=None, is_test=False):
@@ -24,6 +24,11 @@ class CustomMelanomaDataset(Dataset):
         self.sex_encoder = LabelEncoder().fit(self.metadata['sex'])
         self.site_encoder = LabelEncoder().fit(self.metadata['anatom_site_general_challenge'])
 
+        # Separate malignant and benign samples for dynamic augmentation
+        if not is_test:
+            self.malignant_samples = self.metadata[self.metadata['benign_malignant'] == 'malignant']
+            self.benign_samples = self.metadata[self.metadata['benign_malignant'] == 'benign']
+            
     def __len__(self):
         return len(self.metadata)
 
@@ -36,7 +41,8 @@ class CustomMelanomaDataset(Dataset):
         sex = self.metadata.iloc[idx, 2]
         age = self.metadata.iloc[idx, 3]
         site = self.metadata.iloc[idx, 4]
-        
+        benign_malignant = self.metadata.iloc[idx, 5]
+
         # Encode categorical metadata
         sex_encoded = self.sex_encoder.transform([sex])[0]
         site_encoded = self.site_encoder.transform([site])[0]
@@ -48,18 +54,24 @@ class CustomMelanomaDataset(Dataset):
         }
         
         # Convert metadata to tensor
-        metadata_tensor = torch.tensor([metadata['sex'], metadata['age'], 
-        metadata['site']], dtype=torch.float)
-        if self.is_test:
-            benign_malignant = self.metadata.iloc[idx, 5]
-            target = torch.tensor(1 if benign_malignant == 'malignant' else 0, dtype=torch.float)
+        metadata_tensor = torch.tensor([metadata['sex'], metadata['age'], metadata['site']], dtype=torch.float)
         
+        # Define target
+        target = torch.tensor(1 if benign_malignant == 'malignant' else 0, dtype=torch.float)
+        
+        # Apply augmentations dynamically for malignant samples during training
+        if not self.is_test and benign_malignant == 'malignant':
+            augment_transform = transforms.Compose([
+                transforms.RandomHorizontalFlip(),
+                transforms.RandomVerticalFlip(),
+                transforms.RandomRotation(30),
+                transforms.ColorJitter(brightness=0.3, contrast=0.3, saturation=0.3, hue=0.1),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+            ])
+            image = augment_transform(image)
         else:
-            # For training data, include benign_malignant and target
-            benign_malignant = self.metadata.iloc[idx, 5]
-            target = torch.tensor(1 if benign_malignant == 'malignant' else 0, dtype=torch.float)
-
-        if self.transform:
-            image = self.transform(image)
+            if self.transform:
+                image = self.transform(image)
 
         return image, metadata_tensor, target
